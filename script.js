3ae7edfc33db1989a89289557ce5a8c98bccf6b9
Irene Riportella <riportellairene@gmail.com>
2022-07-29 19:25:39 +0200

    clase 04 completada

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-import checkComplete from './components/checkComplete.js';
+// import checkComplete from './components/checkComplete.js';
 import deleteIcon from './components/deleteIcon.js';
 
-const btn = document.querySelector('[data-form-btn]');
-console.log(btn);
+(() => {
+  const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const content = `<div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
+  const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    //backticks
+
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
 
-  task.innerHTML = content;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
 
-  list.appendChild(task);
+    // task.innerHTML = content;
 
-  const taskContent = document.createElement('div');
+    task.appendChild(deleteIcon());
+  };
 
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
+  //Arrow functions o funciones anonimas
+  btn.addEventListener('click', createTask);
 
-  task.appendChild(taskContent);
-  task.appendChild(deleteIcon());
-};
+  const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+  };
 
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+  const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+  };
+})();
